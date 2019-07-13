70b9cc0070ad91d599aadf27b60aac9fd559026b
Nontawat wutthifaey <mistidea9@gmail.com>
2019-07-13 14:14:24 +0700

    เพิ่มหน้าเทส

--- index.js
+++ index.js
@@ -5,5 +5,8 @@ const port = 3000
 app.get('/', (req, res) => res.send('Nontawat Wutthifaey'))
 app.get('/about', (req, res) => res.send('หน้า about'))
 app.get('/contact', (req, res) => res.send('หน้า ติดต่อเรา'))
+app.get('/test', (req, res) => {
+    res.json({message: "test"})
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
